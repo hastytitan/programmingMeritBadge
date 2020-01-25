147efa8584ff2d6cd07063fc1818ae5b47b9950c
Hastytitan <alexgator9@outlook.com>
2020-01-24 20:25:50 -0600

    wip

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const lowTempFWarning float64 = 0
+const highTempFWarning float64 = 100
+const maxLoop int = 5
 
 func main() {
-	fmt.Println("we all love the bulldogs in my house")
+	var f, c float64 = 0, 0
+	var err error
+	reader := bufio.NewReader(os.Stdin)
+	for i := 0; i < maxLoop; i++ {
+		fmt.Println("\nEnter a temperature in Fahrenheit: ")
+		text, _ := reader.ReadString('\n')
+		text = strings.Trim(text, " \n\r")
+		if f, err = strconv.ParseFloat(text, 64); err != nil {
+			fmt.Println("\nNot a valid number")
+			os.Exit(-1)
+		}
 
+		c = (f - 32) * 5 / 9
+		fmt.Printf("\nThe temperature in Celsius is: %v", c)
+
+		if f > highTempFWarning {
+			fmt.Println("\nRemember to hydrate")
+		}
+		if f < lowTempFWarning {
+			fmt.Println("\nRemember to pack Long underwear")
+		}
+	}
+	os.Exit(-1)
 }
