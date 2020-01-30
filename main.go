47b731b78f93b2bc2b3f33618d9ce0fa68169a31
Hastytitan <alexgator9@outlook.com>
2020-01-29 19:34:19 -0600

    wip

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,25 +13,45 @@ const highTempFWarning float64 = 100
 const maxLoop int = 5
 
 func main() {
-	var f, c float64 = 0, 0
+	var f, c, wc, wcel float64 = 0, 0, 0, 0
+
 	var err error
-	reader := bufio.NewReader(os.Stdin)
+	var fText, wcText string
+
 	for i := 0; i < maxLoop; i++ {
 		fmt.Println("\nEnter a temperature in Fahrenheit: ")
-		text, _ := reader.ReadString('\n')
-		text = strings.Trim(text, " \n\r")
-		if f, err = strconv.ParseFloat(text, 64); err != nil {
+		fmt.Scanln(&fText)
+
+		fmt.Println("\nEnter the windspeed: ")
+		fmt.Scanln(&wcText)
+
+		fText = strings.Trim(fText, " \n\r")
+		if f, err = strconv.ParseFloat(fText, 64); err != nil {
 			fmt.Println("\nNot a valid number")
 			os.Exit(-1)
 		}
-		// wc := 13.2 + 0.6215*tempF + (0.3965*tempF-11.37)*math.Pow(tempWc, 0.16)
-		c = (f - 32) * 5 / 9
-		fmt.Printf("\nThe temperature in Celsius is: %v", c)
 
-		if f > highTempFWarning {
+		wcText = strings.Trim(wcText, " \n\r")
+		var ws float64
+		if ws, err = strconv.ParseFloat(wcText, 64); err != nil {
+			fmt.Println("\nNot a valid number")
+			os.Exit(-1)
+		}
+
+		wc = 35.74 + 0.6215*f - 35.75*math.Pow(ws, 0.16) + 0.4275*f*math.Pow(ws, 0.16)
+
+		c = (f - 32) * 5 / 9
+		wcel = (wc - 32) * 5 / 9
+
+		fmt.Printf("\nThe temperature in Celsius is: %0.2f but feels like: %0.2f in Farenheit (%0.2f Celsius)", c, wc, wcel)
+
+		if f > 100 || wc > 100 {
 			fmt.Println("\nRemember to hydrate")
 		}
-		if f < lowTempFWarning {
+		if (f > 60 && f < 75) || (wc > 60 && wc < 75) {
+			fmt.Println("\ngo have fun outside!!!")
+		}
+		if f < 0 || wc < 0 {
 			fmt.Println("\nRemember to pack Long underwear")
 		}
 	}
